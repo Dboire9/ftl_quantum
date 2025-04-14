f4fda0401596a63f954be91b63d4a3d523d955c9
Dorian VM <dorian.boire98@gmail.com>
2025-04-14 11:58:23 +0200

    Feat : Added print

--- ex04/Deutsch_Jozsa.py
+++ ex04/Deutsch_Jozsa.py
@@ -1,7 +1,7 @@
-from qiskit import QuantumCircuit, transpile, QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit
 from qiskit_aer import Aer
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from qiskit.visualization import plot_histogram
 
 
@@ -15,8 +15,8 @@ def main():
 	dj_circuit.barrier()
 
 
-	balanced_oracle = oracle_b(n + 1)
-	# const_oracle = oracle_c(n)
+	balanced_oracle = example_oracle(n + 1)
+	const_oracle = oracle_c(n+1)
 	
 	
 	dj_circuit = dj_circuit.compose(balanced_oracle)
@@ -34,10 +34,14 @@ def main():
 	print(counts)
 	plot_histogram(counts)
 	plt.show()
+	for key, value in counts.items():
+		if '1' in key:
+			return print("balanced")
+	return print("constant")
 
 def oracle_b(nb_qubits):
 	balanced_oracle = QuantumCircuit(nb_qubits)
-	for i in range(1):
+	for i in range(3):
 		balanced_oracle.cx(i,3)
 	balanced_oracle.barrier()
 	# print(balanced_oracle)
@@ -45,7 +49,7 @@ def oracle_b(nb_qubits):
 
 def oracle_c(nb_qubits):
 	const_oracle = QuantumCircuit(nb_qubits)
-	const_oracle.x(4)
+	const_oracle.x(3)
 	const_oracle.barrier()
 	print(const_oracle)
 	return const_oracle
